1685678c1cf3e1d38f356657645eea79bc3b0821
Danielsilva200319 <89894235+Danielsilva200319@users.noreply.github.com>
2023-12-13 13:15:24 -0500

    new

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-function imgSlider(anything) {
-    document.querySelector('.verde').src = anything;
-}
 function changeColor(colorTitulo) {
     const titulo = document.querySelector('.star');
     titulo.style.color = colorTitulo;
